54f1a0bdd96c6e4348586da867bedc6dd9680545
wantz <wantz@foxmail.com>
2023-06-22 09:45:02 +0800

    关键字搜图 V1.1.0更新

--- preload.js
+++ preload.js
@@ -1,60 +1,27 @@
-const request = require('request');
-
-function search(keyword) {
-    let url = 'https://cn.bing.com/images/vsasync?q=' + keyword
-    let table = document.querySelector('table');
-    table.innerHTML = ''
-    request.responseType = 'json';
-    fetch(url).then(response => response.json()).then(data => {
-        imgs = data['results'];
-        let index = 0
-        let new_tr = document.createElement('tr')
-        for (let i = 0; i < imgs.length; i++) {
-            if (0 == index % 3) {
-                new_tr = document.createElement('tr')
-            }
-            let new_td = document.createElement('td')
-            let link = document.createElement('a');
-            let new_img = document.createElement('img')
-            new_img.src = imgs[i].imageUrl;
-            link.href = imgs[i].imageUrl;
-            link.download = imgs[i].imageUrl;
-            link.appendChild(new_img);
-            new_td.appendChild(link);
-            new_tr.appendChild(new_td);
-            if (2 == index % 3) {
-                table.appendChild(new_tr)
-            }
-            index = index + 1;
-        }
-    })
-}
-
+/*
+ * @Description: 
+ * @Author: Wantz
+ * @Email: wantz@foxmail.com
+ * @Date: 2023-06-21 23:43:32
+ * @LastEditTime: 2023-06-22 09:29:59
+ */
 utools.onPluginEnter(({ code, type, payload }) => {
-    document.querySelector('.info').style.display = ''
-    document.querySelector('table').innerHTML = ''
     const keyword = payload;
     if (keyword === null || keyword == "") {
         return
     }
     var word = '';
     utools.setSubInput(({ text }) => {
-        if (text == null || text == "") {
-            document.querySelector('.info').style.display = '';
-            document.querySelector('table').innerHTML = ''
-        }
         word = text;
     }, placeholder = '输入关键字搜索图片')
     window.addEventListener('keydown', (e) => {
         if (e.code === 'Enter') {
             if (word.length > 0) {
-                document.querySelector('.info').style.display = 'none';
-                search(word);
-            } else {
-                document.querySelector('.info').style.display = ''
+                window.app._instance.ctx.search(word);
+                window.app._instance.data['keyword'] = word;
+                text = ''
             }
-        }
-
+        } 
     })
 
 })
